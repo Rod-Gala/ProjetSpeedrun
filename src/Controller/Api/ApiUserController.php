6b25bb2cfa35c37f1294e3ce65d1f0ca338f5ec1
Rod <Rodolphe.davidg@gmail.com>
2022-06-06 17:10:57 +0200

    Ajout de commentaire

--- src/Controller/Api/ApiUserController.php
+++ src/Controller/Api/ApiUserController.php
@@ -7,18 +7,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+//Bonsoir ceci est un commentaire
 class ApiUserController extends AbstractController{
     
     public function userSearch(Request $request, UserRepository $userRepo): Response
     {
+        //Récupération du username de puis la request
         $name = $request->query->get("username");
 
+        //Récupération des resultats de la query
         $queryResults = $userRepo->getUsernameFromFragUsername($name);
 
         for($i = 0; $i < count($queryResults) ; $i++){
             $queryResults[$i]['url'] = $this->generateUrl('user.show', ['username' => $queryResults[$i]['username']]);
         }
 
+        //Encodage et return de la reponse
         $jsonResults = json_encode($queryResults);
         $response = new Response();
         $response->setContent($jsonResults);
